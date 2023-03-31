b6dc7fa1555ab42d101d70ca857da58ea31127dd
Abir <abirhossain9@iut-dhaka.edu>
2023-03-31 18:54:50 +0600

    Adds rest api for books

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import com.example.demo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/insert")
+    public Book saveBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @GetMapping("")
+    public List<Book> findAll() {
+        return bookService.findAll();
+    }
+}
+
+
---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,82 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "short_description")
+    private String shortDescription;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private BookStatus status = BookStatus.available;
+
+    // ... getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
+}
+
+enum BookStatus {
+    available,
+    borrowed,
+}
+
+
