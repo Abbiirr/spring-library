f30ec39f9b708ce493afe7bfcad4e782cb76a3f6
Abir <abirhossain9@iut-dhaka.edu>
2023-04-03 15:32:27 +0600

    Adds a user model

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,111 @@
+package com.example.demo.model;
+
+import com.example.demo.enums.Role;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private int id;
+    private String email;
+    private String password;
+    private String username;
+    private Role role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String accessToken;
+    private String status;
+
+    // Constructor, getters, setters
+
+public User(int id, String email, String password, String username, Role role, LocalDateTime createdAt, LocalDateTime updatedAt, String accessToken, String status) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.accessToken = accessToken;
+        this.status = status;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+
+
+}
+
