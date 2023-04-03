d2617bfab257f3d45a04b9009e71a25f04a2990e
Abir <abirhossain9@iut-dhaka.edu>
2023-04-03 11:52:08 +0600

    Adds a method in book repository to search and filter

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookById(Long id);
 
      List<Book> findAll();
+
+     @Query("SELECT b FROM Book b WHERE (b.name LIKE %:query% OR b.author LIKE %:query% OR b.genre LIKE %:query% OR b.shortDescription LIKE %:query%) AND (:author is null OR b.author = :author) AND (:genre is null OR b.genre = :genre)")
+    List<Book> searchBooks(@Param("query") String query, @Param("author") String author, @Param("genre") String genre);
+
 
 }
