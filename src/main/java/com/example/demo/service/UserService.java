1aa55ad967a7837f09b621c38f60ee432330dca8
Abir <abirhossain9@iut-dhaka.edu>
2023-04-03 17:33:17 +0600

    Changes <Optional> User to User

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,10 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -33,11 +33,24 @@ public class UserService {
         return userRepository.existsByUsername(username);
     }
 
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+
+
     public void saveUser(User user) {
     LocalDateTime now = LocalDateTime.now();
     user.setCreatedAt(now);
     user.setUpdatedAt(now);
     userRepository.save(user);
 }
+
+    public void updateUser(User existingUser) {
+    LocalDateTime now = LocalDateTime.now();
+    existingUser.setUpdatedAt(now);
+    userRepository.save(existingUser);
+
+    }
 }
 
