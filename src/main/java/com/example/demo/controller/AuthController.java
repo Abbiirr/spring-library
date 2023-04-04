7497308ff7df672f841f1425292642568e8240d3
Abir <abirhossain9@iut-dhaka.edu>
2023-04-04 15:08:46 +0600

    Adds logout controller

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,21 +2,21 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.enums.Role;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Optional;
 
@@ -85,7 +85,7 @@ Key key = new SecretKeySpec(keyBytes, "HmacSHA256");
     .claim("id", existingUser.getId())
     .claim("username", user.getUsername())
     .claim("role", existingUser.getRole())
-    .signWith(SignatureAlgorithm.HS256, key)
+    .signWith(SignatureAlgorithm.HS256, "My cat can generate a better password than this".getBytes())
     .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 24 hours
     .compact();
 
@@ -95,6 +95,33 @@ Key key = new SecretKeySpec(keyBytes, "HmacSHA256");
   userService.updateUser(existingUser);
 
   return ResponseEntity.status(HttpStatus.OK).body(token);
+}
+
+@GetMapping("/logout")
+public ResponseEntity<String> logout(@RequestHeader("Authorization") String token) {
+  try {
+    // Extract the token string from the Authorization header
+    String[] tokenParts = token.split("\\s+");
+    String authToken = tokenParts[1];
+
+    // Parse the token and get the user ID
+    Claims claims = Jwts.parser()
+                   .setSigningKey("My cat can generate a better password than this".getBytes())
+                   .parseClaimsJws(authToken)
+                   .getBody();
+
+    String username = claims.get("username", String.class);
+    User existingUser = userService.findByUsername(username);
+    if (existingUser == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+    }
+    existingUser.setAccessToken(null);
+    existingUser.setStatus("offline");
+    userService.updateUser(existingUser);
+    return ResponseEntity.status(HttpStatus.OK).body("Successfully logged out");
+  } catch (JwtException ex) {
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+  }
 }
 
 
